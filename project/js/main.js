da9bbda1d73dbb4e701feb41439d30caa1df15c0
Kent Bozarth <kent@qvsslimited.com>
2019-08-16 03:12:44 -0600

    Added search bar code to main.js

--- project/js/main.js
+++ project/js/main.js
@@ -2,14 +2,17 @@ $(document).ready(function(){
   getWeather();
 })
 
-function getWeather() {
-   var url = "https://api.openweathermap.org/data/2.5/weather?q=Boston&units=imperial&APPID="+apiKey;
+function getWeather(searchQuery) {
+   var url = "https://api.openweathermap.org/data/2.5/weather?qsearchQuery+"&units=imperial&APPID="+apiKey;
 
 $.ajax(url,{success: function(data){
   $(".city").text(data.name);
   $(".temp").text(data.main.temp);
-
 }})
 }
 
+function searchWeather() {
+  var searchQuery =$(",search").val(); void
+  getWeather(searchQuery)
+}
 
