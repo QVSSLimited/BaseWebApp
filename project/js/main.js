d0b5f3b461f98e3bac7be0f2096626be999d65db
Kent Bozarth <kent@qvsslimited.com>
2019-08-16 02:28:43 -0600

    troubleshoot console on api

--- project/js/main.js
+++ project/js/main.js
@@ -6,8 +6,7 @@ function getWeather() {
    var url = "https://api.openweathermap.org/data/2.5/weather?q=Boston&APPID="+apiKey;
 
 $.ajax(url,{success: function(data){
-  $(".city").text(data.name);
-  $(".temp").text(data.temp);
-
+  console.log(data);
 }})
 }
+
